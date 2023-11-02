441c8252f3391c87ac2b5fc2505b2c89f4e0d4b0
MajaA95 <apostolovamaja95@gmail.com>
2023-11-02 21:05:24 +0100

    Project

--- PizzaApp/PizzaApp.Domain/Entities/Order.cs
+++ PizzaApp/PizzaApp.Domain/Entities/Order.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,16 @@ namespace PizzaApp.Domain.Entities
 {
     public class Order
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set;}
+        public int OrderPrice { get; set;}
+
+        [ForeignKey("User")]
+        public string UserId = string.Empty;
+
+        public List <Pizza>? Pizzas = new List<Pizza>();
+
+
     }
 }
--- PizzaApp/PizzaApp.Domain/Entities/Pizza.cs
+++ PizzaApp/PizzaApp.Domain/Entities/Pizza.cs
@@ -1,6 +1,8 @@
 ﻿using PizzaApp.Domain.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +11,19 @@ namespace PizzaApp.Domain.Entities
 {
     public class Pizza
     {
-        public int Id { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; } 
+                                    
         public int Price { get; set; }
         public string Name { get; set; }
+
+        [ForeignKey("User")]
+        public string UserId = string.Empty;  
+
         public List <Ingredients> Ingredients = new List<Ingredients> ();
+
+        public Order Order { get; set; }
         
     }
 }
