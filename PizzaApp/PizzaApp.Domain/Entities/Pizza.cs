bfda5b1d043ffc1921404c8ca7c5781e79829c2c
MajaA95 <apostolovamaja95@gmail.com>
2023-11-02 20:01:19 +0100

    Project

---
+++ PizzaApp/PizzaApp.Domain/Entities/Pizza.cs
@@ -0,0 +1,18 @@
+﻿using PizzaApp.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaApp.Domain.Entities
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public int Price { get; set; }
+        public string Name { get; set; }
+        public List <Ingredients> Ingredients = new List<Ingredients> ();
+        
+    }
+}
---
+++ PizzaApp/PizzaApp.Domain/Enums/Ingredients.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaApp.Domain.Enums
+{
+    public enum Ingredients
+    {
+        Pepperoni = 1,
+        Mozarella,
+        Tomatoes,
+        Garlic,
+        Onion,
+        Oregano
+    }
+}
